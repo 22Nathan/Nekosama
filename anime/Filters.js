75437fe65ca3764e1a19bacf9e6603eb371b52a1
pperez91100 <105656438+pperez91100@users.noreply.github.com>
2023-06-22 12:13:07 +0200

    Update Filters.js

--- anime/Filters.js
+++ anime/Filters.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 
 const Filters = ({
@@ -12,26 +12,38 @@ const Filters = ({
   handleFilterByFormat,
 }) => {
   return (
-    <View style={styles.filter}>
-      <Picker style={styles.picker} selectedValue={filterYear} onValueChange={handleFilterByYear}>
-        <Picker.Item label="All Years" value="" />
-        {availableYears.map((year) => (
-          <Picker.Item key={year} label={year.toString()} value={year.toString()} />
-        ))}
-      </Picker>
-      <Picker style={styles.picker} selectedValue={filterStatus} onValueChange={handleFilterByStatus}>
-        <Picker.Item label="All Status" value="" />
-        <Picker.Item label="In Progress" value="1" />
-        <Picker.Item label="Completed" value="2" />
-      </Picker>
-      <Picker style={styles.picker} selectedValue={filterFormat} onValueChange={handleFilterByFormat}>
-        <Picker.Item label="All Formats" value="" />
-        <Picker.Item label="OAV" value="ova" />
-        <Picker.Item label="TV" value="tv" />
-        <Picker.Item label="FILM" value="m0v1e" />
-        <Picker.Item label="Special" value="special" />
-      </Picker>
-    </View>
+    <ScrollView
+      horizontal={true}
+      showsHorizontalScrollIndicator={true}
+      persistentScrollbar={true}
+    >
+      <View style={styles.filter}>
+        <View style={styles.pickerContainer}>
+          <Picker style={styles.picker} selectedValue={filterYear} onValueChange={handleFilterByYear}>
+            <Picker.Item label="All Years" value="" />
+            {availableYears.map((year) => (
+              <Picker.Item key={year} label={year.toString()} value={year.toString()} />
+            ))}
+          </Picker>
+        </View>
+        <View style={styles.pickerContainer}>
+          <Picker style={styles.picker} selectedValue={filterStatus} onValueChange={handleFilterByStatus}>
+            <Picker.Item label="All Status" value="" />
+            <Picker.Item label="In Progress" value="1" />
+            <Picker.Item label="Completed" value="2" />
+          </Picker>
+        </View>
+        <View style={styles.pickerContainer}>
+          <Picker style={styles.picker} selectedValue={filterFormat} onValueChange={handleFilterByFormat}>
+            <Picker.Item label="All Formats" value="" />
+            <Picker.Item label="OAV" value="ova" />
+            <Picker.Item label="TV" value="tv" />
+            <Picker.Item label="FILM" value="m0v1e" />
+            <Picker.Item label="Special" value="special" />
+          </Picker>
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 
@@ -42,10 +54,13 @@ const styles = StyleSheet.create({
     justifyContent: 'space-around',
     marginBottom: 16,
   },
-  picker: {
+  pickerContainer: {
     flex: 1,
-    height: 40,
     marginHorizontal: 8,
+  },
+  picker: {
+    height: 40,
+    width: 170
   },
 });
 
