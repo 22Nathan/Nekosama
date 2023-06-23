78c44459ecd82848584c5174402c103d45826624
pperez91100 <105656438+pperez91100@users.noreply.github.com>
2023-06-23 14:09:01 +0200

    update connexion

--- anime/Connexion.js
+++ anime/Connexion.js
@@ -1,85 +1,107 @@
 import React from 'react';
-import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, Animated } from 'react-native';
 import { CommonActions } from '@react-navigation/native';
 import { ADRESSEIP } from './.CONST.js';
-import { useState } from 'react';
-import axios from 'axios';
+import { useState, useEffect, useRef } from 'react';
 
 const Connexion = ({ navigation }) => {
-
   const [inputEmail, setInputEmail] = useState('');
   const [inputPassword, setInputPassword] = useState('');
+  const [error, setError] = useState('');
+  const [showError, setShowError] = useState(false);
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    if (showError) {
+      Animated.timing(fadeAnim, {
+        toValue: 1,
+        duration: 500,
+        useNativeDriver: true,
+      }).start();
+
+      const timer = setTimeout(() => {
+        setShowError(false);
+        Animated.timing(fadeAnim, {
+          toValue: 0,
+          duration: 500,
+          useNativeDriver: true,
+        }).start();
+      }, 3000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [showError]);
 
   const handleInputChangeEmail = (text) => {
     setInputEmail(text);
-  }
+    setError('');
+  };
+
   const handleInputChangePassword = (text) => {
     setInputPassword(text);
-  }
+    setError('');
+  };
 
   const handleConnexion = async () => {
-
-    const url   = `http://${ADRESSEIP}:8000/user/login?email=${inputEmail}&password=${inputPassword}`
-    const urlwt = `http://${ADRESSEIP}:8000/user/login`
-    // console.log(url); 
+    const url = `http://${ADRESSEIP}:8000/user/login?email=${inputEmail}&password=${inputPassword}`;
+    const urlwt = `http://${ADRESSEIP}:8000/user/login`;
 
     try {
-
-      const response = await fetch( urlwt, {
+      const response = await fetch(urlwt, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
-          email:inputEmail,
-          password:inputPassword
-        })
-      })
+          email: inputEmail,
+          password: inputPassword,
+        }),
+      });
 
       if (response.ok) {
         const result = await response.json();
         console.log('Response:', result);
-        if( result ){
+        if (result) {
           navigation.dispatch(
             CommonActions.reset({
               index: 0,
-              routes: [{ 
-                name: 'ListAnime',
-                params: {
-                  isAdmin:result.user.isAdmin || false
-                }
-              }]
+              routes: [
+                {
+                  name: 'ListAnime',
+                  params: {
+                    isAdmin: result.user.isAdmin || false,
+                  },
+                },
+              ],
             })
-          )
+          );
         }
       } else {
-        console.log('Utilisateur non existant');
+        setError('Email ou mot de passe incorrect');
+        setShowError(true);
       }
-
     } catch (error) {
       console.log(error);
     }
-
-
-    // .then(response => console.log( response.data) )
-    // .then(result => {
-    //   console.log('Response:', result);
-    //   // if( result ){
-    //   //   navigation.dispatch(
-    //   //     CommonActions.reset({
-    //   //       index: 0,
-    //   //       routes: [{ name: 'ListAnime' }]
-    //   //     })
-    //   //   );
-    //   // }
-    // })
-    // .catch(error => { console.error('Error:', error); });
-
   };
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Connexion</Text>
-      <TextInput style={styles.input} onChangeText={handleInputChangeEmail} placeholder="Email" />
-      <TextInput style={styles.input} onChangeText={handleInputChangePassword} placeholder="Mot de passe" secureTextEntry={true} />
+      <TextInput
+        style={styles.input}
+        onChangeText={handleInputChangeEmail}
+        placeholder="Email"
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={handleInputChangePassword}
+        placeholder="Mot de passe"
+        secureTextEntry={true}
+      />
+      {showError && (
+        <Animated.View style={[styles.errorContainer, { opacity: fadeAnim }]}>
+          <Text style={styles.errorText}>{error}</Text>
+        </Animated.View>
+      )}
       <TouchableOpacity style={styles.button} onPress={handleConnexion}>
         <Text style={styles.buttonText}>Se connecter</Text>
       </TouchableOpacity>
@@ -120,6 +142,17 @@ const styles = StyleSheet.create({
     color: 'white',
     fontSize: 16,
     fontWeight: 'bold',
+  },
+  errorContainer: {
+    backgroundColor: 'red',
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderRadius: 8,
+    marginBottom: 10,
+  },
+  errorText: {
+    color: 'white',
+    fontSize: 14,
   },
 });
 
