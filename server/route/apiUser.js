53df7bb968df4bf97f2ae23ea2eb27042ae2844a
22Nathan <temp22m@gmail.com>
2023-06-23 12:48:01 +0200

    :alien:

--- server/route/apiUser.js
+++ server/route/apiUser.js
@@ -3,6 +3,7 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../model/user')
+const bcrypt = require('bcrypt')
 
 
 router.post('/user/login', async (req, res) => {
@@ -39,6 +40,53 @@ router.post('/user/signup', async (req, res) => {
         } else { res.status(500).json({ message: 'err' }) }
     } catch (error) { res.status(400).json({ message: error.message }) }
 
+})
+
+
+
+router.delete('/user/delete', async (req, res) => {
+
+    console.log(req.body);
+
+    try {
+        // const { email } = req.body.email
+        const deletedUser = await User.findOneAndDelete({ email: req.body.email })
+
+    if (!deletedUser) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    return res.status(200).json({ message: 'User deleted successfully' });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+
+})
+
+
+
+router.put('/user/update', async (req, res) => {
+
+    console.log(req.body);
+
+    try {
+      const { email, password } = req.body;
+
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      const updatedUser = await User.findOneAndUpdate({email:email}, { password: hashedPassword, email: email }, {
+        new: true,
+      });
+  
+      if (!updatedUser) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+      return res.status(200).json({ message: 'User updated successfully', user: updatedUser });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: 'Internal server error' });
+    }
 })
 
 
